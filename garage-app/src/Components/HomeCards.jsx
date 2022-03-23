25d125941ca0fd75d93f3e9882d77de3b9567173
Shubham_Samanta <Shubham.Samanta1@ibm.com>
2022-03-23 16:01:34 +0530

    @HomeCards.jsx just removed target="_blank" so that new page opens in the same tab

--- garage-app/src/Components/HomeCards.jsx
+++ garage-app/src/Components/HomeCards.jsx
@@ -37,7 +37,8 @@ const HomeCards = (props) => {
         <h5 className="card-title">{props.el.title}</h5>
         <p className="card-text">{props.el.content}</p>
         
-        <NavLink className="btn btn-primary me-2 mt-3" target="_blank" to={'/'+props.el.id} >Open in new Tab</NavLink>
+        {/* <NavLink className="btn btn-primary me-2 mt-3" target="_blank" to={'/'+props.el.id} >Open in new Tab</NavLink> */}
+        <NavLink className="btn btn-primary me-2 mt-3" to={'/'+props.el.id} >Open in new Tab</NavLink>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <button onClick={likeClicked} className="fa fa-thumbs-up btn btn-primary fs-4  mt-3 ">{props.el.likes}</button>
